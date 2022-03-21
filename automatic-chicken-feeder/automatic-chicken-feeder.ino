2016cb31f16ebeeb6781736ad43e816480d18ae1
Von <vonshirco.vs@gmail.com>
2022-03-21 15:48:18 +0300

    Update automatic-chicken-feeder.ino

--- automatic-chicken-feeder/automatic-chicken-feeder.ino
+++ automatic-chicken-feeder/automatic-chicken-feeder.ino
@@ -1,4 +1,11 @@
 //Project Overview
+
+//Definig Libraries
+#include <DS3231.h> //For RTC Module
+#include <Servo.h> //For Servor Motor
+#include <LiquidCrystal.h> //For 16*2 LCD
+#include <Keypad.h> //For 4*4 Matrix Keypad
+
 void setup() {
   // put your setup code here, to run once:
 
