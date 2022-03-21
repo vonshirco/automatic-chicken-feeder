9044ce24c67d241c91a32afe4958b2ef3c54dd8e
Von <vonshirco.vs@gmail.com>
2022-03-21 16:12:05 +0300

    Update automatic-chicken-feeder.ino

--- automatic-chicken-feeder/automatic-chicken-feeder.ino
+++ automatic-chicken-feeder/automatic-chicken-feeder.ino
@@ -6,6 +6,16 @@
 #include <LiquidCrystal.h> //For 16*2 LCD
 #include <Keypad.h> //For 4*4 Matrix Keypad
 
+//Defining the keymap for the 4*4 matrix keypad and assigning the Arduino pins for the Row and Columns of keypad.
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = { 2, 3, 4, 5};
+byte colPins[COLS] = { 6, 7, 8, 9};
+
 void setup() {
   // put your setup code here, to run once:
 
